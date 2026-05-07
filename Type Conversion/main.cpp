d2c3d009ca2a2f5d10f2eccd021a7ecf359d18b1
Ferno-prog <arandomemail751@gmail.com>
2026-05-07 12:44:15 +0200

    Type Conversion

--- Type Conversion/main.cpp
+++ Type Conversion/main.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
-    std::cout << "Hallo World"; // hellooioi
+    double d = 42.99;
+    float f = static_cast<float>( d );
+    int i = static_cast<int>( f );
+    int c = static_cast<char>( i );
+    bool b = static_cast<bool>( c );
+
+    std::cout << d << '\n'
+    << f << '\n'
+    << i << '\n'
+    << c << '\n'
+    << b << '\n';
+
 }
